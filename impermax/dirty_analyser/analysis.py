7438ebf437fcdf5120da3fd9411e82811b57bd87
Kevin <digitalexmachina@gmail.com>
2021-12-12 21:22:08 +0100

    added stablecoin contracts and plotting

--- impermax/dirty_analyser/analysis.py
+++ impermax/dirty_analyser/analysis.py
@@ -4,6 +4,8 @@ from functools import cached_property
 from matplotlib import pyplot
 from pandas import DataFrame
 
+from impermax.dirty_analyser.enums import AvalancheStables, STABLECOIN_TICKERS, MoonRiverStables, ArbitrumStables, \
+    MaticStables, EthereumStables
 from impermax.dirty_analyser.extract_data import load_all_csv
 from impermax.fetcher.enums import ImpermaxURLS
 
@@ -42,6 +44,7 @@ class NumpyAnalytics:
     _the_data: list[dict]
 
     def __post_init__(self):
+        self._freq = '1D'
         self._df = DataFrame(data=self._the_data)
         self._df.set_index('datetime', inplace=True)
         self._interpolate_df()
@@ -51,7 +54,7 @@ class NumpyAnalytics:
         return self._df
 
     def _interpolate_df(self) -> None:
-        target_idx = self._df.asfreq('1D').index
+        target_idx = self._df.asfreq(self._freq).index
         self._df = self._df.reindex(self._df.index.union(target_idx))
 
         self._df['blockchain'] = self._df['blockchain'].interpolate(method='bfill')
@@ -71,20 +74,31 @@ class NumpyAnalytics:
 
         self._df = self._df.reindex(target_idx)
 
+def is_stablecoin(side: DataFrame) -> bool:
+    return any(stable in side['ticker'][0] for stable in STABLECOIN_TICKERS)
 
-def analyse_pools(pool_addresses: list[str]):
+def plot_pools(pool_addresses: list[str], **plot_kwargs):
     for pool in pool_addresses:
         filtered_data = PoolDataFilter(contract=pool)
         left = NumpyAnalytics(filtered_data.left).df
         right = NumpyAnalytics(filtered_data.right).df
-        right['supply_apr'].plot()
-        pyplot.show()
+        if is_stablecoin(right):
+            label = ', '.join([right['ticker'][0], right['pair'][0], right['dex'][0], right['blockchain'][0]])
+            right[label] = right['supply_apr']  # changing column name for plot
+            right[label].plot(**plot_kwargs)
+        if is_stablecoin(left):
+            label = ', '.join([left['ticker'][0], left['pair'][0], left['dex'][0], left['blockchain'][0]])
+            left[label] = left['supply_apr']  # changing column name for plot
+            left[label].plot(**plot_kwargs)
 
 
 if __name__ == '__main__':
-    pools = [
-        '0xcb8f6c44c85f4a55f243df1d1f339a52a452aca5',  # MATIC/WETH POLYGON
-        '0x542ccc962097b184ca12a46030d967a735342cf8',  # USDC/DAI POLYGON
-    ]
-    analyse_pools(pools)
+    common_kwargs = dict(legend=True)
+    # common_kwargs = dict()
+    # plot_pools(MoonRiverStables.list(), color='yellow', **common_kwargs)
+    plot_pools(AvalancheStables.list(), **common_kwargs)
+    # plot_pools(MaticStables.list(), color='pink', **common_kwargs)
+    # plot_pools(ArbitrumStables.list(), color='grey', **common_kwargs)
+    # plot_pools(EthereumStables.list(), color='black', **common_kwargs)
+    pyplot.show()
     ...
